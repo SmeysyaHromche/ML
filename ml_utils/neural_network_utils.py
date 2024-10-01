c0e93b5ccf641e0ee692deffca92249ac612491c
SmeysyaHromche <MyrKykhta@gmail.com>
2024-10-01 22:24:57 +0200

    upd activation function decoder+old configs and make optim of loss data storing

--- ml_utils/neural_network_utils.py
+++ ml_utils/neural_network_utils.py
@@ -3,7 +3,9 @@ from typing import List
 def activation_decode(act_f_str:str):
             if act_f_str == 'identity':
                 return nn.Identity()
-            elif act_f_str == 'sig':
+            elif act_f_str == 'silu':
                 return nn.SiLU()
+            elif act_f_str == 'sig':
+                  return nn.Sigmoid()
             else:
                 return nn.ReLU()
