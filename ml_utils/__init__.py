a709bd5d48de11363063bbf7c517a6c8df57a9a7
SmeysyaHromche <MyrKykhta@gmail.com>
2024-10-15 16:49:17 +0200

    upd ml utils

--- ml_utils/__init__.py
+++ ml_utils/__init__.py
@@ -1,3 +1,3 @@
 from ml_utils.config_utils import parse_config
-from ml_utils.neural_network_utils import activation_decode
+from ml_utils.neural_network_utils import activation_decode, decode_dataset
 from ml_utils.vizual import vizualization
--- ml_utils/neural_network_utils.py
+++ ml_utils/neural_network_utils.py
@@ -1,4 +1,5 @@
 import torch.nn as nn
+from torchvision import datasets
 from typing import List
 def activation_decode(act_f_str:str):
             if act_f_str == 'identity':
@@ -9,3 +10,13 @@ def activation_decode(act_f_str:str):
                   return nn.Sigmoid()
             else:
                 return nn.ReLU()
+            
+def decode_dataset(name:str):
+    if name == 'mnist':
+        return datasets.MNIST
+    elif name == 'cifar10':
+        return datasets.CIFAR10
+    elif name == 'imgnet':
+        return datasets.ImageNet
+    else:
+        raise Exception(f'Unknow dataset with name {name}') 
