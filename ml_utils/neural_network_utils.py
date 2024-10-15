14d1f9a4956420cc9cf3f28f4c8561dbf201509f
SmeysyaHromche <MyrKykhta@gmail.com>
2024-10-15 17:06:13 +0200

    activation_decode -> decode_activation

--- ml_utils/neural_network_utils.py
+++ ml_utils/neural_network_utils.py
@@ -1,7 +1,7 @@
 import torch.nn as nn
 from torchvision import datasets
 from typing import List
-def activation_decode(act_f_str:str):
+def decode_activation(act_f_str:str):
             if act_f_str == 'identity':
                 return nn.Identity()
             elif act_f_str == 'silu':
