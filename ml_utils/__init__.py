a4a56f8ea4694020d24b3e003d1ae9f4fdc2c7a9
SmeysyaHromche <MyrKykhta@gmail.com>
2024-10-20 17:08:20 +0200

    create CNN autoencoder for SSL

--- ml_utils/__init__.py
+++ ml_utils/__init__.py
@@ -1,3 +1,3 @@
 from ml_utils.config_utils import parse_config
 from ml_utils.neural_network_utils import decode_activation, decode_dataset
-from ml_utils.vizual import vizualization
+from ml_utils.vizual import vizualization, print_step
--- ml_utils/vizual.py
+++ ml_utils/vizual.py
@@ -2,9 +2,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.ticker import FormatStrFormatter
 
-def vizualization(x:np.array, x_label:str, y:np.array, y_label:str, title:str):
+def vizualization(x:np.array, x_label:str, y:np.array, y_label:str, title:str)->None:
     plt.plot(x, y)
     plt.title(title)
     plt.xlabel(x_label)
     plt.ylabel(y_label)
     plt.show()
+
+def print_step(msg:str)->None:
+    print('=====================================================================')    
+    print(f'\t\t\t {msg}')
+    print('=====================================================================')
